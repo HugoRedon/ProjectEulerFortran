7d58717e62caa51daa7eb93df6df0f424d899694
HugoRedon <hugo_redon_@hotmail.com>
2015-07-15 18:30:33 -0500

    fifth problem solved

--- five.f
+++ five.f
@@ -1,8 +1,36 @@
       PROGRAM five
-      
+      integer arr(1:20)
+      logical res,isDiv
+      integer countr
+      countr = 20  
+      res = .false.
+      do i = 1,20
+      arr(i) = i
+C	  print*, arr(i)
+      end do	
+C      print *, arr	  
+      do while(res .eqv. .false.)
+      res =  isDiv(countr,arr,size(arr))
+      countr =  1+ countr
+      end do
+      print*,res, " ", (countr-1)
+      read(*,*)
       end program five
 	  
-      logical function isDiv(x,arr)
-      integer x
-      integer arr(2)
+      logical function isDiv(x, arr,n)
+      integer x, td,ton
+      integer arr(n)
+      logical logE
+
+C      print *, size(arr)
+      do j = 1, n
+      td = mod(x,j)
+      logE = .not.(td.eq.0)
+C      print *, logE
+      if (logE) then
+      isDiv =.false.
+      return
+      end if
+      end do
+      isDiv = .true.
       end
